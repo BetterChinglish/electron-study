45325047eb71f17dae95924a02245e0941929d3d
BetterChinglish <2199840669@qq.com>
2024-08-03 15:56:15 +0800

    format

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 
 const { handleSend } = window.myAPI;
 const resultArea = document.querySelector('#result-area');
-document.querySelector('#send-message').addEventListener('click', ()=>{
+document.querySelector('#send-message')
+  .addEventListener('click', ()=>{
     handleSend();
-})
+  })
